f8f9b2ce2f66940a1e9252fd055277291fe8833c
Hirotaka-0926 <abehirotaka94@gmail.com>
2025-01-17 02:53:45 +0900

    station 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
-import { store } from "./store"
-import {createRoot} from "react-dom/client";
+import { store } from "./store";
+import { createRoot } from "react-dom/client";
 
 const rootEl = document.getElementById("root");
 const root = createRoot(rootEl);
@@ -15,8 +15,7 @@ root.render(
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </Provider>,
-
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -11,26 +11,30 @@ import { SignUp } from "../pages/SignUp";
 import { EditList } from "../pages/EditList";
 
 export const Router = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
+  const auth = useSelector((state) => state.auth.isSignIn);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/signin" element={<SignIn/>} />
-        <Route exact path="/signup" element={<SignUp/>} />
+        <Route exact path="/signin" element={<SignIn />} />
+        <Route exact path="/signup" element={<SignUp />} />
         {auth ? (
           <>
-            <Route exact path="/" element={<Home/>} />
-            <Route exact path="/task/new" element={<NewTask/>} />
-            <Route exact path="/list/new" element={<NewList/>} />
-            <Route exact path="/lists/:listId/tasks/:taskId" element={<EditTask/>} />
-            <Route exact path="/lists/:listId/edit" element={<EditList/>} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/task/new" element={<NewTask />} />
+            <Route exact path="/list/new" element={<NewList />} />
+            <Route
+              exact
+              path="/lists/:listId/tasks/:taskId"
+              element={<EditTask />}
+            />
+            <Route exact path="/lists/:listId/edit" element={<EditList />} />
           </>
         ) : (
           <Navigate to="/signin" />
         )}
-        <Route element={<NotFound/>} />
+        <Route element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
